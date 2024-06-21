b0ce73485b8672da38ccf064aaa135b89f30ccd5
bondgh0954 <otengdevops@gmail.com>
2024-06-21 20:21:31 +0200

    modify jenkinsfile

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -9,21 +9,21 @@ class Docker implements Serializable{
     }
 
 
-    buildJar(){
+    def buildJar(){
         script.echo 'building jar file from maven application........'
         script.sh 'mvn clean package'
     }
-    buildImage(String imageName){
+    def buildImage(String imageName){
         script.echo 'building application into docker image.......'
         script.sh "docker build -t $imageName ."
     }
-    dockerLogin(){
+    def dockerLogin(){
         script.echo 'logging in to private docker repository.....'
         script.withCredentials([script.usernamePassword(credentialsId:'dockerhub-credentials',usernameVariable:'USER',passwordVariable:'PASS')]){
             script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
         }
     }
-    pushImage(String imageName){
+    def pushImage(String imageName){
         script.sh "docker push $imageName"
     }
 
